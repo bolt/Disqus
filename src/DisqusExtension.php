a9f36b4007635868ea357f730063deba69c10582
Bob den Otter <bobdenotter@gmail.com>
2018-05-30 09:10:46 +0200

    Update Disqus extension for the 2018 version of their embeddable code.

--- src/DisqusExtension.php
+++ src/DisqusExtension.php
@@ -4,8 +4,10 @@ namespace Bolt\Extension\Bolt\Disqus;
 
 use Bolt\Asset\Snippet\Snippet;
 use Bolt\Asset\Target;
+use Bolt\Collection\Bag;
 use Bolt\Extension\SimpleExtension;
 use Bolt\Version;
+use Symfony\Component\HttpFoundation\Request;
 use Twig\Markup;
 
 /**
@@ -26,36 +28,36 @@ class DisqusExtension extends SimpleExtension
     }
 
     /**
-     * @param string $title
-     *
      * @return Markup
      */
-    public function disqus($title = '')
+    public function disqus()
     {
         $config = $this->getConfig();
         $app = $this->getContainer();
+        $request = Request::createFromGlobals();
+
+        if (!$config->get('disqus_name')) {
+            return new \Twig_Markup("<p>Please set the 'Disqus Short name' in <code>app/config/extensions/disqus.bolt.yml</code>.</p>", 'UTF-8');
+        }
 
         $html = <<< EOM
         <div id="disqus_thread"></div>
-        <script type="text/javascript">
-            var disqus_shortname = '%shortname%';
-            %title%var disqus_url = '%url%';
-
-            (function () {
-                var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-                dsq.src = 'https://' + disqus_shortname + '.disqus.com/embed.js';
-                (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
-            })();
+        <script>
+        var disqus_config = function () {
+            this.page.url = '%url%';
+            this.page.identifier = '%id%';
+        };
+        (function() {
+            var d = document, s = d.createElement('script');
+            s.src = 'https://%shortname%.disqus.com/embed.js';
+            s.setAttribute('data-timestamp', +new Date());
+            (d.head || d.body).appendChild(s);
+        })();
         </script>
-        <noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
-        <a href="http://disqus.com" class="dsq-brlink">comments powered by <span class="logo-disqus">Disqus</span></a>
-
+        <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
 EOM;
-        if ($title != '') {
-            $title = "var disqus_title = '" . htmlspecialchars($title, ENT_QUOTES, 'UTF-8') . "';\n";
-        } else {
-            $title = '';
-        }
+
+        $id = $request->server->get('REQUEST_URI');
 
         if ((version_compare(Version::forComposer(), 3.2, '>='))) {
             $canonical = $app['canonical']->getUrl();
@@ -63,16 +65,14 @@ EOM;
             $canonical = $app['resources']->getUrl('canonicalurl');
         }
 
-        $html = str_replace('%shortname%', $config['disqus_name'], $html);
+        $html = str_replace('%shortname%', $config->get('disqus_name'), $html);
         $html = str_replace('%url%', $canonical, $html);
-        $html = str_replace('%title%', $title, $html);
+        $html = str_replace('%id%', $id, $html);
 
         return new Markup($html, 'UTF-8');
     }
 
     /**
-     * @param $link
-     *
      * @return Markup
      */
     public function disquslink($link)
@@ -81,18 +81,17 @@ EOM;
         $app = $this->getContainer();
 
         $script = <<< EOM
-<script type="text/javascript">
-var disqus_shortname = '%shortname%';
-(function () {
-var s = document.createElement('script'); s.async = true;
-s.type = 'text/javascript';
-s.src = 'https://' + disqus_shortname + '.disqus.com/count.js';
-(document.getElementsByTagName('HEAD')[0] || document.getElementsByTagName('BODY')[0]).appendChild(s);
-}());
-</script>
-
+        <script type="text/javascript">
+        var disqus_shortname = '%shortname%';
+        (function () {
+        var s = document.createElement('script'); s.async = true;
+        s.type = 'text/javascript';
+        s.src = 'https://' + disqus_shortname + '.disqus.com/count.js';
+        (document.getElementsByTagName('HEAD')[0] || document.getElementsByTagName('BODY')[0]).appendChild(s);
+        }());
+        </script>
 EOM;
-        $script = str_replace('%shortname%', $config['disqus_name'], $script);
+        $script = str_replace("%shortname%", $config->get('disqus_name'), $script);
 
         $asset = new Snippet();
         $asset->setCallback($script)
@@ -110,4 +109,10 @@ EOM;
     {
         return ['disqus_name' => 'boltcm'];
     }
+
+    protected function getConfig()
+    {
+        return $this->config = new Bag(parent::getConfig());
+    }
+
 }
